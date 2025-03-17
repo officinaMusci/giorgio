cee281cad1c504c6782c53328be9edc04c7b7489
Danilo Musci <danilomusci@gmail.com>
2025-03-17 18:39:16 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='giorgio',
-    version='0.1.1',
+    version='0.2.0',
     description='A lightweight micro-framework for script automation with a GUI.',
     author='Danilo Musci',
     author_email='officina@musci.ch',
